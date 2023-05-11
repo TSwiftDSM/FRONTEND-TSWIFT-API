fd034b856ba269ef623a6a6f94e1dbbc3b36c59d
dmssjk <dmssjk@gmail.com>
2023-05-11 08:40:20 -0300

    correcao icone cadastro realizado

--- src/pages/admin/CadastroRealizado.jsx
+++ src/pages/admin/CadastroRealizado.jsx
@@ -9,7 +9,11 @@ const CadastroRealizado = () => {
         <h3 className="text-white">Cadastrar</h3>
       </div>
       <div className="card mx-auto col-lg-6 p-5">
-        <FontAwesomeIcon icon="fa-solid fa-check" size="2xl" m />
+        <FontAwesomeIcon
+          icon="fa-solid fa-check"
+          size="2xl"
+          style={{ color: "#21a676" }}
+        />
         <span className="mb-5 text-center ">
           Cadastro realizado com sucesso.
         </span>
