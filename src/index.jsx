10546a99684a727670b55ff004fcacdc4926d716
otavioabreu27 <otavio.abreu96@gmail.com>
2023-05-11 09:54:40 -0300

    ajustando a tela pra sempre ser do tamanho da vp

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/scss/main.scss";
+import "./assets/fa";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./router";
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
