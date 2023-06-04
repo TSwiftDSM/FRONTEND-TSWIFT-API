d7ba1ac101017243bcb8de27f8d80a3cba7eed1f
Jackles kerley <jackleskerley@yahoo.com.br>
2023-06-04 16:46:49 -0300

    ajuste estilização do status pedido

--- src/pages/admin/AlterarPedido.jsx
+++ src/pages/admin/AlterarPedido.jsx
@@ -147,11 +147,11 @@ const NovoPedido = () => {
   }
 
   return (
-    <div>
+    <div className="container-cards">
       <div className="mb-4">
         <h3 className="text-white">Novo Pedido</h3>
       </div>
-      <div className="card p-5 col-lg-6 mx-auto">
+      <div className="card-cadastro p-5 col-lg-6 mx-auto">
         <div className="mb-4">
           <Link to={"/admin/pedidos"}>
             <FontAwesomeIcon icon="fa-solid fa-angle-left" className="me-2" />
